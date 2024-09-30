11bd33c3de560844cc1b4fc008397709cc526d6f
sahir ruiz <sahirruiz4@gmail.com>
2024-09-30 16:23:00 -0500

    Refactor: reorganize minor things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrimengSidebarModule } from './components/primeng-sidebar/primeng-sidebar.module';
+import { PrimengSidebarModule } from './shared/components/primeng-sidebar/primeng-sidebar.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderModule } from './components/header/header.module';
-import { UsersComponent } from './components/users/users.component';
-import { SettingsComponent } from './components/settings/settings.component';
-import { ReportsComponent } from './components/reports/reports.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { DashboardModule } from './components/dashboard/dashboard.module';
-import { RegisterActorsComponent } from './components/register-actors/register-actors.component';
-import { RegisterActorsModule } from './components/register-actors/register-actors.module';
+import { HeaderModule } from './shared/components/header/header.module';
+import { UsersComponent } from './pages/users/users.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { DashboardComponent } from './shared/components/dashboard/dashboard.component';
+import { DashboardModule } from './shared/components/dashboard/dashboard.module';
+import { RegisterActorsComponent } from './pages/register-actors/register-actors.component';
+import { RegisterActorsModule } from './pages/register-actors/register-actors.module';
 
 @NgModule({
   declarations: [AppComponent, UsersComponent, SettingsComponent, ReportsComponent],
